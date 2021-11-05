8f108813bfa8b21f4c7b20f31c9c88bf42f9a31a
Taemin27 <56300365+Taemin27@users.noreply.github.com>
2021-11-05 20:33:51 +0900

    Menu UI
    -menu ui bitmap updated
    -controll with rotary encoder

--- T-Glasses_3.0.ino
+++ T-Glasses_3.0.ino
@@ -1,6 +1,7 @@
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <splash.h>
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <RotaryEncoder.h>
 #include <SPI.h>
 
 #include "bitmaps.h"
@@ -9,6 +10,13 @@
 #define TFT_RST 9
 #define TFT_DC 8
 
+#define PIN_IN1 5
+#define PIN_IN2 4
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
+void checkPosition(){
+  encoder.tick();
+}
+
 
 const uint16_t BLACK = 0x0000;
 const uint16_t WHITE = 0xffff;
@@ -16,51 +24,132 @@ const uint16_t BLUE = 0x001f;
 const uint16_t RED = 0xf800;
 const uint16_t YELLOW = 0xffe0;
 const uint16_t GREEN = 0x07e0;
-
-
+const uint16_t color = WHITE;
 Adafruit_ST7735 display = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
-bool timerActive = false;
+boolean timerActive = true;
 int timerHour = 0;
-int timerMinute = 1;
+int timerMinute = 0;
 int timerSecond = 0;
 
-int stopWatchActive = false;
+boolean stopWatchActive = false;
 int stopWatchHour = 0;
 int stopWatchMinute = 0;
 int stopWatchSecond = 0; 
 
-unsigned long timerCurrentMillis = 0;
-unsigned long stopWatchCurrentMillis = 0;
+boolean encoderButton = false;
+
+unsigned long timerPreviousMillis = 0;
+unsigned long stopWatchPreviousMillis = 0;
+
+String currentPage = "menuHome";
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(6, INPUT_PULLUP);
   display.initR(INITR_MINI160x80);
   display.setRotation(3);
   display.invertDisplay(true);
+
+  attachInterrupt(digitalPinToInterrupt(PIN_IN1), checkPosition, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PIN_IN2), checkPosition, CHANGE);
 
   display.setCursor(0, 0);
   display.setTextSize(3);
   display.setTextColor(WHITE, BLACK);
   display.fillScreen(BLACK);
   
-  display.drawBitmap(0, 0, boot_logo, 160, 80, BLUE);
+  display.drawBitmap(0, 0, boot_logo, 160, 80, color);
   delay(2000);
+  display.fillScreen(BLACK); 
+  menuHome();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  fnTimer();
-  fnStopWatch();
+  static int pos = 0;
+  encoder.tick();
+  int newPos = encoder.getPosition() / 2;
+  while (pos != newPos) { //Encoder Rotated
+    int dir = (int)(encoder.getDirection());
+    if(dir == 1) {
+      Serial.println("Right");
+      if(currentPage == "menuHome") {
+        menuTimer();
+      }
+      else if(currentPage == "menuTimer") {
+        menuStopWatch();
+      }
+      else if(currentPage == "menuStopWatch") {
+        menuNote();
+      }
+      else if(currentPage == "menuNote") {
+        menuSettings();
+      }
+    }
+    else if(dir == -1) {
+      if(currentPage == "menuTimer") {
+        menuHome();
+      }
+      else if(currentPage == "menuStopWatch") {
+        menuTimer();
+      }
+      else if(currentPage == "menuNote") {
+        menuStopWatch();
+      }
+      else if(currentPage == "menuSettings") {
+        menuNote();
+      }
+    }
+    pos = newPos;
+  }
+
+  int encoderButtonValue = digitalRead(6);
+  if(encoderButtonValue == LOW && encoderButton == false) {
+    encoderButton = true;
+    Serial.println("Button pressed");
+    
+  }
+  else if(encoderButtonValue == HIGH && encoderButton == true) {
+    encoderButton = false;
+  }
 }
 
-void uiHome() {
+void menuHome() {
+  currentPage = "menuHome";
+  display.fillScreen(BLACK);
+}
+
+void menuTimer() {
+  currentPage = "menuTimer";
+  display.fillScreen(BLACK);
+  display.drawBitmap(0, 0, ui_timer, 160, 80, color);
+  
   
 }
 
+void menuStopWatch() {
+  currentPage = "menuStopWatch";
+  display.fillScreen(BLACK);
+  display.drawBitmap(0, 0, ui_stopWatch, 160, 80, color);;
+}
+
+void menuNote() {
+  currentPage = "menuNote";
+  display.fillScreen(BLACK);
+  display.drawBitmap(0, 0, ui_note, 160, 80, color);
+}
+
+void menuSettings() {
+  currentPage = "menuSettings";
+  display.fillScreen(BLACK);
+  display.drawBitmap(0, 0, ui_settings, 160, 80, color);
+}
+
 void fnTimer() {
-  if(millis() - timerCurrentMillis >= 1000) {
+  unsigned long currentMillis = millis();
+  if(currentMillis - timerPreviousMillis >= 1000) {
     if((timerHour == 0) && (timerMinute == 0) && (timerSecond == 0)){
       
     }
@@ -80,12 +169,19 @@ void fnTimer() {
       }
     }
     Serial.println("Timer " + String(timerHour) + ":" + String(timerMinute) + ":" + String(timerSecond));
-    timerCurrentMillis = millis();
+    if(currentPage == "home") {
+      display.setCursor(5, 5);
+      display.setTextSize(1);
+      display.setTextColor(color, BLACK);
+      display.print(String(timerHour) + ":" + String(timerMinute) + ":" + String(timerSecond));
+    }
+    timerPreviousMillis += 1000;
   }
 }
 
 void fnStopWatch() {
-  if(millis() - stopWatchCurrentMillis >= 1000) {
+  unsigned long currentMillis = millis();
+  if(currentMillis - stopWatchPreviousMillis >= 1000) {
     if(stopWatchSecond < 59) {
       stopWatchSecond ++;
     } else{
@@ -100,6 +196,12 @@ void fnStopWatch() {
       }
     }
     Serial.println("StopWatch " + String(stopWatchHour) + ":" + String(stopWatchMinute) + ":" + String(stopWatchSecond));
-    stopWatchCurrentMillis = millis();
+    if(currentPage == "home") {
+      display.setCursor(5, 5);
+      display.setTextSize(1);
+      display.setTextColor(color, BLACK);
+      display.print(String(stopWatchHour) + ":" + String(stopWatchMinute) + ":" + String(stopWatchSecond));
+    }
+    stopWatchPreviousMillis += 1000;
   }
 }
